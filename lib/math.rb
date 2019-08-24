88c682ebcdc9b8cbf24881d47312f36da8b72b02
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-08-24 15:00:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
 end
 
 def modulo(num1, num2)
